19b77e6fb964a4a87e46094c11826b59259e59ed
44David <david.soane@outlook.com>
2024-08-18 16:14:28 -0600

    Lists files in current directory in the input form

--- main.go
+++ main.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"errors"
-
 	"github.com/charmbracelet/huh"
+	"os/exec"
+	"log"
 )
 
 
 func main() {
 	var file string
 
+
+	out, err := exec.Command("ls").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	huh.NewInput().
-		Title("Start searching").
+		Title(string(out)).
 		Prompt("? ").
 		Validate(func(filename string) error {
 			if filename == "Test" { 
 				return errors.New("this file does not exist")
 			} 
 
-			return nil
+				return nil
 		}).
 		Value(&file).
 		Run()
+	
+	
 }
